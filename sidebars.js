7faf826d6be910ff0e561e30e177e1ef4eaae2d3
HiQSolutions <hmail@hiqsolutions.it>
2025-12-22 10:28:35 +0100

    Add 'le-mie-attivita' to Governance items

--- sidebars.js
+++ sidebars.js
@@ -14,6 +14,7 @@ const sidebars = {
       items: [
         'governance/asset-relationship',
         'governance/budget-costi-management',
+        'governance/le-mie-attivita',
       ],
     },
   ],
